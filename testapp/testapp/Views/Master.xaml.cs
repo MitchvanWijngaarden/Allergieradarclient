b8b9af6447e4bcb9cb150967b3efc8db1238c3d9
Vedad Piric <vedadpiric@MacBook-Pro-van-Vedad.local>
2017-05-15 19:38:44 +0200

    ADD NAVIGATION

---
+++ testapp/testapp/Views/Master.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace testapp.Views
+{
+    public partial class Master : ContentPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+    }
+}
