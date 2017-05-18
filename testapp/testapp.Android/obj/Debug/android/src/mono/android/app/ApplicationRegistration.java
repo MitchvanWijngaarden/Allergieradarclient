b227a5043b85488dac9f44611120c64b83a1ee63
Unknown <MitchvanWijngaarden@gmail.com>
2017-05-18 11:55:40 +0200

    Longtitude lattitude

--- testapp/testapp.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
+++ testapp/testapp.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -5,6 +5,7 @@ public class ApplicationRegistration {
 	public static void registerApplications ()
 	{
 				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("testapp.Droid.MainApplication, testapp.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md557933599a254786be35615d2153791c5.MainApplication.class, md557933599a254786be35615d2153791c5.MainApplication.__md_methods);
 		
 	}
 }
