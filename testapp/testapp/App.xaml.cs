e8060e2a9bbe925da780c53c3f701dad08936618
Bernd Oostrum <berndoostrum@gmail.com>
2017-05-24 11:50:57 +0200

    master verwerkt in loginviews

--- testapp/testapp/App.xaml.cs
+++ testapp/testapp/App.xaml.cs
@@ -6,62 +6,28 @@ using Xamarin.Forms.Xaml;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace testapp
 {
-	public partial class App : Application
-	{
+    public partial class App : Application
+    {
 
         public static bool IsUserLoggedIn { get; set; }
-        public App()
-		{
-			InitializeComponent();
-<<<<<<< HEAD
 
-	
+        public App()
+        {
+            InitializeComponent();
 
             if (!IsUserLoggedIn)
             {
-                MainPage = new NavigationPage(new LoginPage());
+                MainPage = new Views.RootPage();
             }
             else
             {
-                SetMainPage();
+                MainPage = new Views.RootPage();
             }
+
+            
+
         }
 
-		public static void SetMainPage()
-		{
-            Current.MainPage = new TabbedPage
-            {
-                Children =
-                {
-                    new NavigationPage(new ItemsPage())
-                    {
-                        Title = "Browse",
-                        Icon = Device.OnPlatform<string>("tab_feed.png",null,null)
-                    },
-                    new NavigationPage(new AboutPage())
-                    {
-                        Title = "About",
-                        Icon = Device.OnPlatform<string>("tab_about.png",null,null)
-                    },
-                    new NavigationPage(new KlachtenPage())
-                    {
-                        Title = "Klachten",
-                        Icon = Device.OnPlatform<string>("tab_about.png",null,null)
-                    },
-                    new NavigationPage(new LoginPage())
-                    {
-                        Title = "Inloggen",
-                        Icon = Device.OnPlatform<string>("tab_about.png", null,null)
-                    }
-                }
-            };
-        }
-=======
-			MainPage = new Views.RootPage();
-			
-		}
 
-
->>>>>>> dc3c6041b89aaab8fd380ccb7863d0dbc9efbf08
-	}
+    }
 }
