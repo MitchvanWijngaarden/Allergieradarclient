41f324da5319fadbcf2ea9ae8b76e24c5502ef7c
TOUCHTOP-DANNY\Danny <s1096011@student.hsleiden.nl>
2017-06-22 00:03:21 +0200

    no message

---
+++ testapp/testapp/ViewModels/VragenlijstModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testapp.ViewModels
+{
+    class VragenlijstModel
+    {
+        private Dictionary<int, string> myList;
+        public Dictionary<int, string> MyList
+        {
+            get { return myList; }
+            set
+            {
+                myList = value;
+                NotifyPropertyChanged("MyList");
+            }
+        }
+
+        private void LoadData()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                MyList.Add(i, "Item " + i);
+            }
+        }
+    }
+}
