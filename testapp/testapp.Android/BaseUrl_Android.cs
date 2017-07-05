1c3c9f9be44b171295074e56d8aedbd27e557aeb
Bernd Oostrum <berndoostrum@gmail.com>
2017-07-05 18:41:01 +0200

    merge fix

---
+++ testapp/testapp.Android/BaseUrl_Android.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+using WorkingWithWebview.Android;
+using testapp.Views;
+
+[assembly: Dependency (typeof (BaseUrl_Android))]
+namespace WorkingWithWebview.Android 
+{
+	public class BaseUrl_Android : IBaseUrl 
+	{
+		public string Get () 
+		{
+			return "file:///android_asset/";
+		}
+	}
+}
